cd726bcedec6beb2b77076bb2cb224bd1b3bd1be
Your Name <hardsontessi2@gmail.com>
2022-09-18 21:24:07 +0100

    spring security

--- src/main/java/com/hardtech/devoxx/DevoxxApplication.java
+++ src/main/java/com/hardtech/devoxx/DevoxxApplication.java
@@ -1,13 +1,30 @@
 package com.hardtech.devoxx;
 
+import com.hardtech.devoxx.domain.Speaker;
+import com.hardtech.devoxx.domain.SpeakerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DevoxxApplication {
+public class DevoxxApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DevoxxApplication.class, args);
-	}
+    @Autowired
+    private SpeakerRepository speakerRepository;
 
+    public static void main(String[] args) {
+        SpringApplication.run(DevoxxApplication.class, args);
+    }
+
+    void addSpeaker() {
+        Speaker dave = speakerRepository.save(new Speaker("David", "Syer", "dave_syer"));
+        Speaker nicoll = speakerRepository.save(new Speaker("Stéphane", "Nicoll", "stephane_nicoll"));
+        Speaker clozel = speakerRepository.save(new Speaker("brian", "clozel", "brian_clozel"));
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        addSpeaker();
+    }
 }
