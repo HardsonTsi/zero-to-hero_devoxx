8e170987748b541f0425939557ed4c1eef15f607
Your Name <hardsontessi2@gmail.com>
2022-09-18 19:31:41 +0100

    SpeakerRepository RestResource, spring-boot-devtools

--- src/main/java/com/hardtech/devoxx/domain/SpeakerRepository.java
+++ src/main/java/com/hardtech/devoxx/domain/SpeakerRepository.java
@@ -1,12 +1,15 @@
 package com.hardtech.devoxx.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import java.util.Collection;
 
 public interface SpeakerRepository extends JpaRepository<Speaker, Long> {
 
-    Speaker findByTwitter(String twitter);
+    @RestResource(path = "by-twitter")
+    Speaker findByTwitter(@Param("id") String twitter);
 
     Collection<Speaker> findByLastName(String lastName);
 
